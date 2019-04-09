142f63255e837c646b400224eded1a2bc3dc9b03
YoanMorel <yoan.morel@live.fr>
2019-04-09 11:54:49 +0200

    Jasmine la coquine

--- questions.js
+++ questions.js
@@ -1,63 +1,63 @@
 /**
  * Exercice sur les chaines de caractères.
  * Trouvez la façon de faire la plus optimal.
- * Il peut y avoir plusieur façon de faire. 
+ * Il peut y avoir plusieur façon de faire.
  */
 var tailleString = function (texte) {
-    return 'A completer';
+    return texte.length;
 }
 var remplaceECar = function (texte) {
-    return 'A completer';
+    return texte.replace('e', ' ');
 }
 var concatString = function (texte1, texte2) {
-    return 'A completer';
+    return texte1.concat(texte2);
 }
 var afficherCar5 = function (texte) {
-    return 'A completer';
+    return texte.charAt(4);
 }
 var afficher9Car = function (texte) {
-    return 'A completer';
+    return texte.substr(0, 9);
 }
 var majusculeString = function (texte) {
-    return 'A completer';
+    return texte.toUpperCase();
 }
 var minusculeString = function (texte) {
-    return 'A completer';
+    return texte.toLowerCase();
 }
 var SupprEspaceString = function (texte) {
-    return 'A completer';
+    return texte.trim();
 }
 var IsString = function (texte) {
-    return 'A completer';
+    return isNaN(texte);
 }
 var AfficherExtensionString = function (texte) {
-    return 'A completer';
+    return texte.slice(texte.search('jpg'));
 }
 var NombreEspaceString = function (texte) {
-    return 'A completer';
+    return texte.match(/[ ]/g).length;
 }
 var InverseString = function (texte) {
-    return 'A completer';
+    return texte.split('').reverse('').join('');
 }
 
 /**
  * Exercices sur les nombres et les caluls mathématiques
  */
 var calculPuissance = function (x, y) {
-    return 'A completer';
+    return Math.pow(x, y);
 }
 var valeurAbsolue = function (nombre) {
-    return 'A completer';
+    return Math.abs(nombre);
 }
 var valeurAbsolueArray = function (array) {
-    return 'A completer';
+    return array.map(Math.abs);
 }
 var sufaceCercle = function (rayon) {
-    return 'A completer';
+    return Math.round(Math.PI * (rayon * rayon));
 }
 var hypothenuse = function (ab, ac) {
-    return 'A completer';
+    return Math.sqrt(Math.pow(ab, 2) + Math.pow(ac, 2));
 }
 var calculIMC = function (poids, taille) {
-    return 'A completer';
+    return parseFloat((poids / (taille * taille)).toPrecision(4));
 }
